c19a18f2bce75b1cf6ef2e316a76f821c07660bb
Raphael Lupchinski <27raphalupi@gmail.com>
2019-01-03 01:07:08 -0200

    Adding webpack as bundler

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>Test</div>;
+    return <div>Test 2</div>;
   }
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  }
+};
